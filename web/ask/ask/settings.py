272061346aa115304ff1034d80402d026b927c56
alecsbeinar <alecsanderbejnar@gmail.com>
2023-06-22 11:18:47 +0300

    django models

--- web/ask/ask/settings.py
+++ web/ask/ask/settings.py
@@ -77,8 +77,16 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'stepic_web',
+        # 'USER': 'root',
+        # 'PASSWORD': '2004Bnr@04',
+        # 'HOST': '127.0.0.1',  
+        # 'PORT': '3306',  
+        # 'OPTIONS': {  
+        #     'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        # }  
+        'USER': 'box',
     }
 }
 
--- web/ask/qa/models.py
+++ web/ask/qa/models.py
@@ -1,3 +1,32 @@
-from django.db import models
+from django.db import models, connection
+from django.contrib.auth.models import User
 
-# Create your models here.
+class QuestionManager(models.Manager):
+
+    def new(self):
+        return self.order_by("-added_at")
+    
+    def popular(self):
+        return self.order_by("-rating")
+    
+
+class Question(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, related_name='question_like_user')
+
+    objects = QuestionManager()
+
+    def __str__(self) -> str:
+        return f"Title: {self.title}"
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField(blank=True)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    author = models.CharField(max_length=200)
+
