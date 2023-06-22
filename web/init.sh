7e2a5ee846a893e8f58a56c77fb9674ed4ba5c5c
alecsbeinar <alecsanderbejnar@gmail.com>
2023-06-22 11:27:25 +0300

    added init.sh

--- web/init.sh
+++ web/init.sh
@@ -1,10 +1,19 @@
-# sudo apt update
-# sudo apt install python3.5
-# sudo rm /usr/bin/python3
-# sudo ln -s /usr/bin/python3.5 /usr/bin/python3
-# sudo pip3 install gunicorn
-# sudo pip3 install django==2.1
+sudo apt update
+sudo apt install python3.5
+sudo rm /usr/bin/python3
+sudo ln -s /usr/bin/python3.5 /usr/bin/python3
+sudo pip3 install gunicorn
+sudo pip3 install django==2.1
+sudo python3 -m pip install mysqlclient
 
+sudo /etc/init.d/mysql start
+mysql -uroot -e "create database stepic_web;"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON stepic_web.* TO 'box'@'localhost' WITH GRANT OPTION;"
+source ./venv/bin/activate
+cd ask
+python3 manage.py makemigrations
+python3 manage.py migrate
+cd ..
 
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
